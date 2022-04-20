4dab74ccebe6fbcf6bb023eed579c5c56bcc027d
SandaL <smartyalexpro@gmail.com>
2022-04-20 22:24:03 +0300

    Added some styles in pages

--- WW_WPF/Pages/SelectCharacter.xaml.cs
+++ WW_WPF/Pages/SelectCharacter.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WW_WPF.ViewModels;
 
 namespace WW_WPF
 {
@@ -23,6 +24,7 @@ namespace WW_WPF
         public SelectCharacter()
         {
             InitializeComponent();
+            DataContext = new SelectCharacterViewModel();
         }
     }
 }
---
+++ WW_WPF/ViewModels/SelectCharacterViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using WW_WPF.BL;
+
+namespace WW_WPF.ViewModels
+{
+    public class SelectCharacterViewModel : INotifyPropertyChanged
+    {
+        private Character character;
+        private Enemy enemy;
+
+        public Character Character
+        {
+            get { return character; }
+            set { character = value; }
+        }
+        public Enemy Enemy
+        {
+            get { return enemy; }
+            set { enemy = value; }
+        }
+
+        public SelectCharacterViewModel()
+        {
+            character = new Warrior();
+            enemy = new Barbarian();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
